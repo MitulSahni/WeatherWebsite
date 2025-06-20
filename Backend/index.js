87a388c8aa02be0b1d95571e6a37559fc614bc7f
Your Name <you@example.com>
2025-06-20 18:43:31 +0530

    Initial commit

---
+++ Backend/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const app=express();
+require('dotenv').config();
+const PORT=process.env.PORT||5000;
+mongoose.connect(process.env.MONGODBURI);
+app.use(cors());
+app.use(express.json());
+const WeatherData=mongoose.model('WeatherData', {
+  city: String,
+  country: String,
+  temperature: Number,
+  description: String,
+  icon: String,
+});
+app.post('/api/weather',async(req,res)=>{
+    try{
+        const {city,country,temperature,description,icon}=req.body;
+        const weatherData=new WeatherData({
+            city,
+            country,
+            temperature,
+            description,
+            icon,
+        });
+        await weatherData.save();
+        res.json({ message:'Weather data saved successfully'});
+    }catch(error){
+        console.error('Error saving weather data:',error);
+        res.status(500).json({error:'Internal Server Error'});
+    }
+});
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+});
